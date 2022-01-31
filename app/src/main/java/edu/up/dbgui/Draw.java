06d534f72fb5918b31fefaed68b6008f761a243a
“[Bryce <[manleyb23@up.edu]>
2022-01-30 20:45:15 -0800

    this is a commit of Files relevent to the project

---
+++ app/src/main/java/edu/up/dbgui/Draw.java
@@ -0,0 +1,46 @@
+package edu.up.dbgui;
+
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Canvas;
+import android.graphics.Paint;
+import android.util.AttributeSet;
+import android.view.SurfaceView;
+
+
+    public class Draw extends SurfaceView {
+        private Bitmap dImage;
+        private Bitmap scaled = null;
+
+        public Draw(Context context) {
+            super(context);
+            initialize();
+        }
+
+        public Draw(Context context, AttributeSet attrs) {
+            super(context, attrs);
+            initialize();
+        }
+
+        private void initialize() {
+            dImage = BitmapFactory.decodeResource(getResources(), R.drawable.db_board);
+            setWillNotDraw(false);
+        }
+
+        @Override
+        public void onDraw(Canvas canvas) {
+            if(scaled == null){
+            scaled = Bitmap.createScaledBitmap(dImage,this.getWidth(),this.getHeight(),false);
+            }
+            canvas.drawBitmap(scaled, 0, 0, new Paint());
+        }
+
+   /*     <edu.up.dbGUI.Draw
+        android:id="@+id/our_first_drawing"
+        android:layout_width="match_parent"
+        android:layout_height="match_parent"
+        tools:ignore="MissingClass" />*/
+    }
+
+
